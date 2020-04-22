51eb46123b8bfa43943c8e74008c82457e09711e
gabrielM01u <gmanzano@miners.utep.edu>
2020-04-22 09:33:11 -0600

    fixed bugs

--- lcdLib/trapezoid_assignment.c
+++ lcdLib/trapezoid_assignment.c
@@ -14,22 +14,22 @@ int main()
     int x_origin, y_origin = 10;
 
     //Rectangle
-    while(y=0; if y<=10; y++){
-      while(x=0; if x<=20; x++){
+    for(y=0; if y<=10; y++){
+      for(x=0; if x<=20; x++){
         drawPixel(x+x_origin,y+y_origin,COLOR_WHITE);
       }
     }
 
     //Right-Most Triangle
-    while(x=0;x<=10;x++){
-      while(y=0;y<=x;y++){
+    for(x=0;x<=10;x++){
+      for(y=0;y<=x;y++){
         drawPixel(x+x_origin+20,y+y_origin+20,COLOR_WHITE);
       }
     }
 
     //Left-Most Triangle
-    while(x=0;x<=10;x++){
-      while(y=0;y<=x;y++){
+    for(x=0;x<=10;x++){
+      for(y=0;y<=x;y++){
         drawPixel(x+x_origin,-y+y_origin;COLOR_WHITE);
       }
     }
