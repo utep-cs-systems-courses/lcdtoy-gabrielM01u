1154082143dcfafbe77e32aaf534f7c473b21046
gabrielM01u <gmanzano@miners.utep.edu>
2020-05-14 12:12:56 -0600

    more updates

--- project/hardware.h
+++ project/hardware.h
@@ -23,6 +23,12 @@ void buzzer_set_period(short cycles);
 void buzzer_off();
 
 //Switches
+#define S1 BIT0
+#define S2 BIT1
+#define S3 BIT2
+#define S4 BIT3
+#define SWITCHES (S1|S2|S3|S4)
+
 void switch_init();
 void switch_interrupt_handler();
 static char switch_update_interrupt_sense();
