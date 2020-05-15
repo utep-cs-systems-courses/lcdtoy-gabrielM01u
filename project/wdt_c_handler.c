bd28f7ee1bdcb95a66c09c715b2be2f15650118f
gabrielM01u <gmanzano@miners.utep.edu>
2020-05-15 05:32:38 -0600

    more updates

--- project/wdt_c_handler.c
+++ project/wdt_c_handler.c
@@ -6,14 +6,10 @@
 
 void __interrupt_vec(WDT_VECTOR) WDT()
 {
-  if(switch_state_changed){
+  if(switch_state_changed == 1){
     and_sr(~0x18);
-    // static short count = 0;
     P1OUT |= LED_GREEN;		      /**< Green LED on when cpu on */
-    //count ++;
-    
     state_machine( s_pressed );
-    
   }
   s_pressed = 0;
   or_sr(0x18);
