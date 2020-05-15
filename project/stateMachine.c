0fd07f874e704a604d296ac1dde2e14764f9e427
gabrielM01u <gmanzano@miners.utep.edu>
2020-05-15 11:13:28 -0600

    more updates

--- project/stateMachine.c
+++ project/stateMachine.c
@@ -6,7 +6,8 @@
 #include <math.h>
 #include <msp430.h>
 #include "hardware.h"
-//#include "visuals.h"
+#include "visuals.h"
+#include "states.h"
 
 int redraw = 0;
 int finished = 0;
@@ -34,80 +35,4 @@ void state_machine(int s){
         default:
             break;
     }
-}
-/**
- * States
- **/
-
-//State 1: turns green on and plays buzzer for 125 cycles, then turns everything off
-void state_1(){
-   int timer = 0;
-    finished = 0;
-    while(timer++ != 35000){
-        enable_green();
-        led_update();
-        buzzer_set_period(2200);
-        //redraw = 1;
-    }
-    disable_green();
-    buzzer_off();
-    led_update();
-    finished = 1;
-}
-
-//State 1: turns red on and plays buzzer with a different tone for 500 cycles, then turns everything off
-void state_2(){
-    int timer = 0;
-    finished = 0;
-    while(timer++ != 35000){
-        enable_red();
-        led_update();
-        buzzer_set_period(1400);
-    }
-    disable_red();
-    buzzer_off();
-    led_update();
-    finished = 1;
-}
-
-//State 3: turns green on and buzzer for 125 cycles, turns everything off, then turns on red and buzzard for 500 cycles, then turns everything off
-void state_3(){
-    int timer = 0;
-    finished = 0;
-    while(timer++ != 100000){
-        enable_red();
-        led_update();
-        buzzer_set_period(1400);
-    }
-    disable_red();
-    led_update();
-    while(timer++ != 200000){
-        enable_green();
-        led_update();
-        buzzer_set_period(2000);
-    }
-    disable_green();
-    buzzer_off();
-    led_update();
-    finished = 1;
-}
-
-//State 4: turns red on and buzzer for 500 cycles, turns everything off, then does the same things as in State 3
-void state_4(){
-    int timer = 0;
-    finished = 0;
-    while(timer++ != 100000){
-        enable_red();
-        enable_green();
-        led_update();
-        buzzer_set_period(200);
-    }
-    while(timer++ != 200000){
-        buzzer_set_period(1800);
-    }
-    disable_red();
-    disable_green();
-    buzzer_off();
-    led_update();
-    finished = 1;
 }
--- project/states.c
+++ project/states.c
@@ -1,7 +1,8 @@
 #include <math.h>
 #include <msp430.h>
 #include "hardware.h"
-//#include "visuals.h"
+#include "visuals.h"
+
 /**
  * States
  **/
@@ -10,11 +11,12 @@
 void state_1(){
    int timer = 0;
     finished = 0;
-    while(timer++ != 50000){
+    while(timer++ != 35000){
         enable_green();
         led_update();
         buzzer_set_period(2200);
-        //redraw = 1;
+        clearScreen(COLOR_BLACK);
+        drawRect(20,20);
     }
     disable_green();
     buzzer_off();
@@ -26,10 +28,12 @@ void state_1(){
 void state_2(){
     int timer = 0;
     finished = 0;
-    while(timer++ != 50000){
+    while(timer++ != 35000){
         enable_red();
         led_update();
         buzzer_set_period(1400);
+        clearScreen(COLOR_BLACK);
+        drawDaimond(30,30);
     }
     disable_red();
     buzzer_off();
@@ -41,10 +45,12 @@ void state_2(){
 void state_3(){
     int timer = 0;
     finished = 0;
-    while(timer++ != 100000){
+    while(timer++ != 30000){
         enable_red();
         led_update();
         buzzer_set_period(1400);
+        clearScreen(COLOR_WHITE);
+        drawPerson(40,40);
     }
     disable_red();
     led_update();
@@ -77,4 +83,42 @@ void state_4(){
     buzzer_off();
     led_update();
     finished = 1;
+}
+
+void drawRect(int x_origin, int y_origin){
+    int x,y;
+ 
+    for(y=0; y<=10; y++){
+        for(x=0;x<=10; x++)
+        drawPixel(x+x_origin,y+y_origin, COLOR_CYAN);
+    }
+}
+
+void drawDaimond(int x_origin, int y_origin){
+    int x,y;
+    
+    for(y=0;y<=10;y++){
+        for(x=0;x<=20;x++){
+            drawPixel(x+x_origin,y+y_origin+40,COLOR_WHITE);
+            drawPixel(-x+x_origin,y+y_origin+20,COLOR_WHITE);
+            drawPixel(x+x_origin,-y+y_origin+40,COLOR_WHITE);
+            drawPixel(-x+x_origin,-y+y_origin+20,COLOR_WHITE);
+        }
+    }
+}
+
+void drawPerson(int x_origin, int y_origin){
+    int x,y;
+    for(y=0; y<8; y++) drawPixel(3+x_origin,y+y_origin,COLOR_FIREBRICK);//torso
+    for(x=0; x<5; x++) drawPixel(x+x_origin, 0+y_origin; COLOR_FIREBRICK);//shoulder
+    for(y=0; y<3;y++) {
+        drawPixel(0+x_origin; y+y_origin; COLOR_FIREBRICK);//left arm
+        drawPixel(5+x_origin; y+y_origin; COLOR_FIREBRICK);//right arm
+    }
+    for(x=0; x<3; x++) drawPixel(x+x_origin+1, 8+y_origin; COLOR_FIREBRICK);//waist
+    for(y=0; y<3;y++) {
+        drawPixel(1+x_origin; y+y_origin+8; COLOR_FIREBRICK);//left leg
+        drawPixel(3+x_origin; y+y_origin+8; COLOR_FIREBRICK);//right leg
+    }
+
 }
