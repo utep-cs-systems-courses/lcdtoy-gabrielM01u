816531d774bbb23fe3fe8f79d42d20ff0f728b95
gabrielM01u <gmanzano@miners.utep.edu>
2020-05-15 11:17:32 -0600

    more updates

--- project/states.c
+++ project/states.c
@@ -10,7 +10,7 @@
 //State 1: turns green on and plays buzzer for 125 cycles, then turns everything off
 void state_1(){
    int timer = 0;
-    finished = 0;
+ //   finished = 0;
     while(timer++ != 35000){
         enable_green();
         led_update();
@@ -21,13 +21,13 @@ void state_1(){
     disable_green();
     buzzer_off();
     led_update();
-    finished = 1;
+//    finished = 1;
 }
 
 //State 1: turns red on and plays buzzer with a different tone for 500 cycles, then turns everything off
 void state_2(){
     int timer = 0;
-    finished = 0;
+//    finished = 0;
     while(timer++ != 35000){
         enable_red();
         led_update();
@@ -38,13 +38,13 @@ void state_2(){
     disable_red();
     buzzer_off();
     led_update();
-    finished = 1;
+//    finished = 1;
 }
 
 //State 3: turns green on and buzzer for 125 cycles, turns everything off, then turns on red and buzzard for 500 cycles, then turns everything off
 void state_3(){
     int timer = 0;
-    finished = 0;
+//    finished = 0;
     while(timer++ != 30000){
         enable_red();
         led_update();
@@ -62,13 +62,13 @@ void state_3(){
     disable_green();
     buzzer_off();
     led_update();
-    finished = 1;
+//    finished = 1;
 }
 
 //State 4: turns red on and buzzer for 500 cycles, turns everything off, then does the same things as in State 3
 void state_4(){
     int timer = 0;
-    finished = 0;
+//    finished = 0;
     while(timer++ != 100000){
         enable_red();
         enable_green();
@@ -82,7 +82,7 @@ void state_4(){
     disable_green();
     buzzer_off();
     led_update();
-    finished = 1;
+//    finished = 1;
 }
 
 void drawRect(int x_origin, int y_origin){
